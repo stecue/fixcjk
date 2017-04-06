21f45995be00b8aa7d835c133172e380f9fa1098
stecue <stecue@gmail.com>
2017-04-06 19:27:08 -0400

    Unset white-space while using justify.

--- FixCJK!.user.js
+++ FixCJK!.user.js
@@ -2,7 +2,7 @@
 // @name              FixCJK!
 // @name:zh-CN        “搞定”CJK！
 // @namespace         https://github.com/stecue/fixcjk
-// @version           1.2.0
+// @version           1.2.1
 // @description       1) Use real bold to replace synthetic SimSun bold; 2) Regular SimSun/中易宋体 can also be substituted; 3) Reassign font fallback list (Latin AND CJK). Browser serif/sans settings are overridden; 4) Use Latin fonts for Latin part in Latin/CJK mixed texts; 5) Fix fonts and letter-spacing for CJK punctuation marks.
 // @description:zh-cn 中文字体和标点设定及修正脚本
 // @author            stecue@gmail.com
@@ -1363,6 +1363,7 @@
         //the blank spaces will be streched but not the "character-spacing" if using align=justify.
         if (window.getComputedStyle(node.parentNode,null).getPropertyValue('text-align').match(/start/) && useJustify===true) {
             node.parentNode.style.textAlign="justify";
+            node.parentNode.style.whiteSpace="normal"; //Need to reset white-space to make justify effective.
         }
         //node.lang="zh";
         node.parentNode.lang="zh";
