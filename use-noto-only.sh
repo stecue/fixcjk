169c7bfb0f0fc6f2b13b691298f2748ae03bb599
stecue <stecue@gmail.com>
2017-05-03 23:28:14 -0400

    Update the "Noto Only" flavor

--- use-noto-only.sh
+++ use-noto-only.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Generate a version of FixCJK that only uses Noto Serif instead of Noto Sans.
 NOTO_ONLY=FixCJK_noto.user.js
-cat FixCJK?.user.js |sed 's/Microsoft YaHei/Noto Sans/g' > $NOTO_ONLY
-sed -i -e 's/Microsoft YaHei/Noto Serif CJK SC/g' $NOTO_ONLY
+cat FixCJK?.user.js |sed 's/Microsoft YaHei/Noto Sans CJK SC/g' > $NOTO_ONLY
