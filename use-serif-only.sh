70bab9b1f77b65437b4e3bad49b986b4ed97f2c4
stecue <stecue@gmail.com>
2017-05-16 18:21:22 -0400

    Move scripts to util

--- use-serif-only.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Generate a version of FixCJK that only uses Noto Serif instead of Noto Sans.
-SERIF_ONLY=FixCJK_serif.user.js
-cat FixCJK_noto.user.js |sed 's/Noto Sans/Noto Serif/g'|sed 's/Source Han Sans/Source Han Serif/g' > $SERIF_ONLY
-#Noto Serif CJK SC only has Light instead of DemiLight.
-sed -i -e 's/ *DemiLight//g' $SERIF_ONLY
